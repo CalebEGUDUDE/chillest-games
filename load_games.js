ee9e342ff305731a49e9f5dd0540a1301fcc3938
CalebEGUDUDE <calebeguy@gmail.com>
2026-05-20 18:13:55 +0000

    updated css for a sec

--- load_games.js
+++ load_games.js
@@ -29,7 +29,7 @@ const customIconInput = document.getElementById('custom-icon');
 // ==========================================
 // VERSION CHECKER
 // ==========================================
-const CURRENT_VERSION = "1.0.4"; // Increment this when you update your local site code
+const CURRENT_VERSION = "1.0.5"; // Increment this when you update your local site code
 
 async function checkProjectVersion() {
     try {
